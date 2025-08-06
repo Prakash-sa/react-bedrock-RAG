352119fe426816646f61cb7ef7fc72a066f1fd28
Prakash-sa <sainiprakash525@gmail.com>
2025-08-05 18:23:11 -0600

    enter

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -23,6 +23,11 @@ export const ChatInput = ({ onSubmit }: ChatInputProps) => {
                     onChange={onChange}
                     id="large-input"
                     className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            submit();
+                        }
+                    }}
                 />
             </div>
             <div className="flex-none pl-2">
